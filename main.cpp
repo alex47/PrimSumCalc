718456748009968e12267c4f372285b9e60aa966
alex47 <alex19930329@gmail.com>
2017-03-23 20:28:27 +0100

    Initial upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <QCoreApplication>
+#include <QThread>
+#include <QtConcurrent/QtConcurrent>
+#include <QVector>
+#include <QFuture>
+#include <iostream>
+
+/*
+ * Checks if the number is prime
+ * Works only on odd numbers and the number must be bigger than 2
+ *
+*/
+bool isPrime(int num)
+{
+    int limit = num / 2 + 1;
+
+        for (int i = 3; i < limit; i += 2)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+}
+
+int numToAdd(int num)
+{
+    if (isPrime(num))
+    {
+        return num;
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    int limit = 0;
+    long long result = 2;
+
+    std::cout << "Number: ";
+    std::cin >> limit;
+
+    QVector <QFuture < int >> futureVec;
+
+    for (int i = 3; i < limit; i+= 2)
+    {
+        futureVec.append(QtConcurrent::run(numToAdd, i));
+    }
+
+    foreach (QFuture <int> futInst, futureVec)
+    {
+        result += futInst.result();
+    }
+
+    std::cout << std::endl << "Result: " << result;
+
+    return a.exec();
+}
